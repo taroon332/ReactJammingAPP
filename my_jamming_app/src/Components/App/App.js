2d4a7796457a07b0e5975c17eb99640b83fbe509
taroon332 <taroonsingh332@gmail.com>
2022-08-20 07:23:14 -0700

    working app.js

---
+++ my_jamming_app/src/Components/App/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import React from 'react';
+
+class App extends React.Component 
+{
+  render()
+  {
+    return(
+        <div>
+            <h1>Ja<span class="highlight">mmm</span>ing</h1>
+            <div class="App">
+                {/* < Search Bar/> */}
+                <div class="App-playlist">
+                    {/* <SearchResults/>  */}
+                    {/* <Playlist/>  */}
+                </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+export default App;
