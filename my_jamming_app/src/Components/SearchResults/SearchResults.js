c1afbbcd1ba7d5b6ecb93f90ccf115275b9d7edd
taroon332 <taroonsingh332@gmail.com>
2022-08-20 09:20:16 -0700

    working searcResult.js

---
+++ my_jamming_app/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,17 @@
+import './SearchResult.css';
+import React from 'react';
+
+class SearchResult extends React.Component 
+{
+  render()
+  {
+    return(
+        <div className="SearchResults">
+            <h2>Results</h2>
+            {/* <!-- Add a TrackList component --> */}
+        </div>
+    );
+  }
+}
+
+export default SearchResult;
